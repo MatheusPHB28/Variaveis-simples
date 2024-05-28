ca1f66be2204a8a47661f875749f4ae8c4488639
MatheusPHB28 <matheus.k.sousa6@aluno.senai.br>
2024-05-28 10:46:57 -0300

    logo logo as ferias bate

---
+++ lista.js
@@ -0,0 +1,23 @@
+let res =  document.getElementById('lista-tarefa')
+let itemLi = document.getElementById('nova-tarefa')
+
+function mostrar() {
+    var li = document.createElement('li')
+    var input = document.createElement('input')
+
+    var remove = document.createElement('button')
+    remove.addEventListener('click',function() {
+        res.removeChild(li)
+})
+
+input.setAttribute('type', 'checkbox')
+
+remove.textContent = 'Remover'
+
+res.appendChild(li).innerHTML = `${itemLi.value}`
+
+li.appendChild(input)
+
+li.appendChild(remove)
+
+}
